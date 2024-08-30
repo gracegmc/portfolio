276f788f61995a4eb5c760b598f3e433ca4d1855
Grace Chen <gracechen@Graces-MBP-5.lan>
2024-08-30 16:52:35 -0700

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
 import './App.css';
 import { Routes, Route} from "react-router-dom";
 import About from "./routes/About";
 import Careers from "./routes/Careers";
 import Home from "./routes/Home";
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
@@ -40,6 +16,5 @@ function App() {
     </>
   );
 }
-export default App;
 
 export default App;
