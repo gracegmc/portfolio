ed3ebf088dfbdfd6d1249cb2f422bc3602910164
Grace Chen <gracechen@Graces-MBP-5.lan>
2024-08-30 16:47:05 -0700

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -22,4 +22,24 @@ function App() {
   );
 }
 
+import './App.css';
+import { Routes, Route} from "react-router-dom";
+import About from "./routes/About";
+import Careers from "./routes/Careers";
+import Home from "./routes/Home";
+import Navbar from './Navbar';
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/careers" element={<Careers />} />
+      </Routes>
+    </>
+  );
+}
+export default App;
+
 export default App;
