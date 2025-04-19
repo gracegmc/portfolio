566e2779f834ee4a890265dee539f239f29e243d
gracegmc <165077364+gracegmc@users.noreply.github.com>
2025-04-19 01:43:39 -0700

    fixed formatting issues with contact form with js

--- global.js
+++ global.js
@@ -76,5 +76,21 @@ if ("colorScheme" in localStorage) {
 select.addEventListener('input', function (event) {
     setColorScheme(event.target.value)
 });
-  
-  
+
+const form = document.querySelector('form');
+
+form?.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // store data of form here
+    const data = new FormData(this);
+
+    //get the mailto part of the url
+    let url = this.action + '?';
+
+    for (let [name, value] of data) {
+        url += `${name}=${encodeURIComponent(value)}&`
+    }
+
+    location.href = url.slice(0, -1);
+});
